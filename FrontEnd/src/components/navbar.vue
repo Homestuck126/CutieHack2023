0eac82209bfad3f84bc48f912c1f81511fda0be6
stormyy00 <jonathan.trujillo0432@gmail.com>
2023-04-29 15:00:25 -0700

    nav

---
+++ FrontEnd/src/components/navbar.vue
@@ -0,0 +1,57 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+
+</script>
+
+<template>
+  <div class="container">
+    <nav class="navbar">
+      <a href="#">Home</a>
+      <a href="#">About</a>
+      <a href="#">Contact</a>
+    </nav>
+    
+  </div>
+</template>
+
+<style>
+.container {
+  max-width: 960px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+.navbar {
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  color: #fff;
+  padding: 20px;
+}
+
+.navbar a {
+  color: #fff;
+  text-decoration: none;
+  margin-right: 20px;
+}
+
+.row {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.column {
+  flex-basis: 33.33%;
+  padding: 10px;
+}
+
+.column h2 {
+  font-size: 24px;
+  margin-bottom: 10px;
+}
+
+.column p {
+  margin-bottom: 10px;
+}
+</style>
