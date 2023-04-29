e1b9d2f193ff903af97d49e6500a912a7e9b6308
stormyy00 <jonathan.trujillo0432@gmail.com>
2023-04-29 14:40:37 -0700

    page work in progress

--- FrontEnd/src/App.vue
+++ FrontEnd/src/App.vue
@@ -5,8 +5,7 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
+    
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
     </div>
@@ -22,10 +21,7 @@ header {
   line-height: 1.5;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+
 
 @media (min-width: 1024px) {
   header {
@@ -34,10 +30,7 @@ header {
     padding-right: calc(var(--section-gap) / 2);
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
+  
   header .wrapper {
     display: flex;
     place-items: flex-start;
--- FrontEnd/src/components/HelloWorld.vue
+++ FrontEnd/src/components/HelloWorld.vue
@@ -1,43 +1,128 @@
-<script setup>
-defineProps({
-  msg: {
-    type: String,
-    required: true
-  }
-})
-</script>
+
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
+
+  <div class="container">
+    <div class="header">
+      <h1>My Personal Projects</h1>
+    </div>
+    <div class="content">
+      <div v-for="project in projects" :key="project.id" class="project-card">
+        <img :src="project.image" alt="Project Image">
+        <h2>{{ project.title }}</h2>
+        <p>{{ project.description }}</p>
+        <div class="links">
+          <a :href="project.liveLink" target="_blank">Live Demo</a>
+          <a :href="project.codeLink" target="_blank">Source Code</a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped>
+<script>
+export default {
+  data() {
+    return {
+      projects: [
+        {
+          id: 1,
+          title: "My First Project",
+          image: "https://placehold.it/300x200",
+          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+          liveLink: "https://example.com",
+          codeLink: "https://github.com/example/my-first-project"
+        },
+        {
+          id: 2,
+          title: "My Second Project",
+          image: "https://placehold.it/300x200",
+          description: "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+          liveLink: "https://example.com",
+          codeLink: "https://github.com/example/my-second-project"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+.container {
+  max-width: 960px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+.header {
+  text-align: center;
+  margin-bottom: 20px;
+  color: green;
+}
+
+.project-card {
+  margin-bottom: 20px;
+}
+
+.project-card img {
+  width: 100%;
+  height: auto;
+  margin-bottom: 20px;
+}
+
+.project-card h2 {
+  font-size: 24px;
+  margin-bottom: 10px;
+}
+
+.project-card p {
+  margin-bottom: 10px;
+}
+
+.links a {
+  display: inline-block;
+  margin-right: 10px;
+}
+
+
+
 h1 {
   font-weight: 500;
   font-size: 2.6rem;
-  top: -10px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
 }
-
-h3 {
+h1::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #ADD8E6;
+  z-index: -1;
+}
+ul {
   font-size: 1.2rem;
+  position: fixed;
+  top: 0;
+  left: 0;
+  
 }
 
 .greetings h1,
-.greetings h3 {
+.greetings ul {
   text-align: center;
 }
 
 @media (min-width: 1024px) {
   .greetings h1,
   .greetings h3 {
-    text-align: left;
+   
   }
 }
+
+
 </style>
